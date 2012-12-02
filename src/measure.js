ec27d733a8bdbcef1e1f36c409a333bc2fa071ad
Daniel Ringwalt <dringw@gmail.com>
2012-12-02 15:16:16 -0500

    Moved IR backend to document.js, added IR classes

--- src/measure.js
+++ src/measure.js
@@ -10,13 +10,16 @@ Vex.Flow.Measure = function(object) {
   if (! object.time || ! object.time.num_beats || ! object.time.beat_value)
     throw new Vex.RERR("ArgumentError",
               "Measure must be initialized with nonzero num_beats and beat_value");
+  this.time = object.time;
+
   // Default values
   this.attributes = {};
-  this.parts = [];
-
-  Vex.Merge(this, object);
-  for (var i = 0; i < this.parts.length; i++)
-    this.parts[i] = new Vex.Flow.Measure.Part(this.parts[i]);
+  this.parts = new Array();
+  if (object.parts instanceof Array) {
+    this.parts.length = object.parts.length;
+    for (var i = 0; i < object.parts.length; i++)
+      this.parts[i] = new Vex.Flow.Measure.Part(object.parts[i]);
+  }
   this.type = "measure";
 }
 
@@ -24,54 +27,106 @@ Vex.Flow.Measure.prototype.setAttributes = function(attributes) {
   Vex.Merge(this.attributes, attributes);
 }
 
+Vex.Flow.Measure.prototype.getNumberOfParts = function(numParts) {
+  return this.parts.length;
+}
 Vex.Flow.Measure.prototype.setNumberOfParts = function(numParts) {
   this.parts.length = numParts;
 }
 
-if (! Vex.Flow.Backend) Vex.Flow.Backend = {};
+Vex.Flow.Measure.prototype.getPart = function(partNum) {
+  return this.parts[partNum];
+}
+Vex.Flow.Measure.prototype.setPart = function(partNum, part) {
+  if (this.parts.length <= partNum)
+    throw new Vex.RERR("ArgumentError", "Call setNumberOfParts before adding part");
+  this.parts[partNum] = new Vex.Flow.Measure.Part(part);
+}
 
 /**
- * Vex.Flow.Backend.IR - return measures from intermediate JSON representation
+ * Vex.Flow.Measure.Part - a single part (which may include multiple staves/voices)
  * @constructor
  */
-Vex.Flow.Backend.IR = function() {
-  this.documentObject = null;
-}
-
-/**
- * "Parse" an existing IR document object (not necessarily an instance of
- *     Vex.Flow.Document).
- * If the object is invalid, a Vex.RuntimeError is thrown.
- * Upon success, no exception is thrown and #isValid returns true.
- *
- * @param object The original document object
- */
-Vex.Flow.Backend.IR.prototype.parse = function(object) {
-  if (! Vex.Flow.Backend.IR.appearsValid(object))
-    throw new Vex.RERR("InvalidArgument", "IR object must be a valid document");
-  
-  this.documentObject = object;
-}
-
-/**
- * Class method.
- * Returns true if the argument appears to a valid document object.
- * Used when automatically detecting VexFlow IR.
- *
- * @return {Boolean} True if object looks like a valid document.
- */
-Vex.Flow.Backend.IR.appearsValid = function(object) {
-  return typeof object == "object" && object.type == "document";
-}
-
-/**
- * Returns true if the passed-in code parsed without errors.
- *
- * @return {Boolean} True if code is error-free.
- */
-Vex.Flow.Backend.IR.prototype.isValid = function() { return this.valid; }
-
-// TODO: getNumberOfMeasures, getMeasure methods
-
 Vex.Flow.Measure.Part = function(object) {
+  if (typeof object != "object")
+    throw new Vex.RERR("ArgumentError", "Invalid argument to constructor");
+  if (! object.time || ! object.time.num_beats || ! object.time.beat_value)
+    throw new Vex.RERR("ArgumentError",
+                       "Constructor requires nonzero num_beats and beat_value");
+  this.time = object.time;
+  if (object.voices instanceof Array) {
+    this.voices = new Array(object.voices.length);
+    for (var i = 0; i < object.voices.length; i++)
+      this.voices[i] = new Vex.Flow.Measure.Voice(object.voices[i]);
+  }
+  else this.voices = new Array();
+
+  this.type = "part";
+}
+
+/**
+ * Vex.Flow.Measure.Voice - a voice which contains notes, etc
+ * @constructor
+ */
+Vex.Flow.Measure.Voice = function(object) {
+  if (typeof object != "object")
+    throw new Vex.RERR("ArgumentError", "Invalid argument to constructor");
+  if (! object.time || ! object.time.num_beats || ! object.time.beat_value)
+    throw new Vex.RERR("ArgumentError",
+                       "Constructor requires nonzero num_beats and beat_value");
+  this.time = object.time;
+  // etc
+  if (object.notes instanceof Array) {
+    this.notes = new Array(object.notes.length);
+    for (var i = 0; i < object.notes.length; i++)
+      this.notes[i] = new Vex.Flow.Measure.Note(object.notes[i]);
+  }
+  else this.notes = new Array();
+
+  this.type = "voice";
+}
+
+/**
+ * Vex.Flow.Measure.Stave - represent one "stave" for one measure
+ * (corresponds to a Vex.Flow.Stave)
+ * @constructor
+ */
+Vex.Flow.Measure.Stave = function(object) {
+  if (typeof object != "object")
+    throw new Vex.RERR("ArgumentError", "Invalid argument to constructor");
+  if (! object.time || ! object.time.num_beats || ! object.time.beat_value)
+    throw new Vex.RERR("ArgumentError",
+                       "Constructor requires nonzero num_beats and beat_value");
+  this.time = object.time;
+  this.modifiers = new Array();
+  if (object.modifiers instanceof Array) {
+    for (var i = 0; i < object.modifiers.length; i++)
+      this.addModifier(object.modifiers[i]);
+  }
+
+  this.type = "stave";
+}
+/**
+ * Adds a modifier (clef, etc.), which is just a plain object with a type
+ * and other properties.
+ */
+Vex.Flow.Measure.Stave.prototype.addModifier = function(modifier) {
+  // TODO: Verify that the modifier is correct
+  this.modifiers.push(modifier);
+}
+
+/**
+ * Vex.Flow.Measure.Note - a single note (includes chords, rests, etc.)
+ * @constructor
+ */
+Vex.Flow.Measure.Note = function(object) {
+  if (typeof object != "object")
+    throw new Vex.RERR("ArgumentError", "Invalid argument to constructor");
+  if (object.keys instanceof Array)
+    // Copy keys array
+    // TODO: check each element
+    this.keys = object.keys.slice();
+  else this.keys = new Array();
+
+  this.type = "note";
 }
